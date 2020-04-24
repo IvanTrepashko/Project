d4de882a7352cce3bad3b82cfd5438e558468a94
IvanTrepashko <goodfellawk@gmail.com>
2020-04-24 22:00:52 +0300

    Configuring API

---
+++ FinancialAssistant/FinancialAssistant/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using NbrbAPI.Models;
+
+namespace FinancialAssistant
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            ApiHelper.InitializeClient();
+            CurrencyProcessor processor = new CurrencyProcessor();
+            var cur = await processor.LoadCurrencies();
+
+            foreach (var currency in cur)
+            {
+                Console.WriteLine(currency);
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
---
+++ FinancialAssistant/FinancialAssistant/Rate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NbrbAPI.Models
+{
+    public class Rate
+    {
+        [Key]
+        public int Cur_ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Cur_Abbreviation { get; set; }
+        public int Cur_Scale { get; set; }
+        public string Cur_Name { get; set; }
+        public decimal? Cur_OfficialRate { get; set; }
+        public override string ToString()
+        {
+            string str;
+            str = $"Currency ID : {Cur_ID}\nDate : {Date.ToString("yyyy-mm-dd")}\nCurrency Abbrev {Cur_Abbreviation}\nCurrency Scale : {Cur_Scale}\nCurrency Name : {Cur_Name}\nCurrency off rate : {Cur_OfficialRate}";
+            return str;
+        }
+    }
+
+    public class RateShort
+    {
+        public int Cur_ID { get; set; }
+        [Key]
+        public System.DateTime Date { get; set; }
+        public decimal? Cur_OfficialRate { get; set; }
+    }
+}
