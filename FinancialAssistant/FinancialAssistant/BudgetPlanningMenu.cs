5c676223aa5a40a8fd270102e78e5c5f72dee075
IvanTrepashko <goodfellawk@gmail.com>
2020-04-28 12:11:23 +0300

    Started budget planning

---
+++ FinancialAssistant/FinancialAssistant/BudgetPlanningMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace FinancialAssistant
+{
+    public static class BudgetPlanningMenu
+    {
+        public static void MainMenu()
+        {
+            while (true)
+            {
+                BudgetPlan budgetPlan = new BudgetPlan();
+                Console.Clear();
+                Console.WriteLine("Welcome to budget planning menu.\n\nWhat do you want to do?\n");
+                Console.WriteLine("1. See my current budget plan.\n2. Create new budget plan.3. Delete current budget plan.");
+                int.TryParse(Console.ReadLine(), out int choice);
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            break;
+                        }
+                    case 2:
+                        {
+                            break;
+                        }
+
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ FinancialAssistant/FinancialAssistant/BudgetSpending.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinancialAssistant
+{
+    public class BudgetSpending
+    {
+        public SpendingCategory Category { get; set; }
+        public double PlannedAmount { get; set; }
+        public double SpentAmount { get; set; }
+
+        public BudgetSpending(int category)
+        {
+            double planned;
+
+            Category = (SpendingCategory)category;
+            Console.WriteLine($"Please, enter planned amount of money for {Category}.");
+            while(!double.TryParse(Console.ReadLine(),out planned) || !planned.IsPositive())
+            {
+                Console.WriteLine("Wrong input. Please, try again.");
+            }
+            PlannedAmount = planned;
+            SpentAmount = 0;
+        }
+
+        public BudgetSpending(int category, double planned, double spent)
+        {
+            Category = (SpendingCategory)category;
+            PlannedAmount = planned;
+            SpentAmount = spent;
+        }
+    }
+}
